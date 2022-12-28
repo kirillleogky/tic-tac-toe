3d662aa00bcbcd155c6105bf219e3fbb526c9128
kirillleogky <kirillmargheriti@gmail.com>
2022-12-28 18:53:43 +0300

    feat: change win combination

--- src/utils/finishGame.ts
+++ src/utils/finishGame.ts
@@ -5,10 +5,10 @@ const winMoveMatrix = [
   [3, 4, 5],
   [6, 7, 8],
   [0, 3, 6],
-  [1, 4, 8],
+  [1, 4, 7],
   [2, 5, 8],
   [0, 4, 8],
-  [2, 4, 6],
+  [2, 4, 6]
 ];
 const finishGame = (
   moves: MoveType[],
